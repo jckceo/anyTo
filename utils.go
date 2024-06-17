6d7c90f0da533cdb93d80833b900b17aa24d797c
Jack <49846464+jckceo@users.noreply.github.com>
2024-06-17 09:01:45 +0200

    Update module to version 1.0.3 with parseString changes

--- utils.go
+++ utils.go
@@ -16,9 +16,34 @@ func removeUnicodeChars(input string) string {
 
 func parseString(s string) string {
 	s = removeUnicodeChars(s) // Remove spaces, tabs, newlines, etc.
-	if strings.Contains(s, ",") { // Remove commas
-		s = strings.Replace(s, ".", "", -1) // Remove the period used as thousands separator
+	// Count occurrences of commas and periods
+	commaCount := strings.Count(s, ",")
+	periodCount := strings.Count(s, ".")
+
+	if commaCount > 1 && periodCount == 1 {
+		// Case: US format with commas as thousand separators and one period as decimal point
+		s = strings.Replace(s, ",", "", -1) // Remove all commas
+	} else if periodCount > 1 && commaCount == 1 {
+		// Case: European format with periods as thousand separators and one comma as decimal point
+		s = strings.Replace(s, ".", "", -1) // Remove all periods
 		s = strings.Replace(s, ",", ".", 1) // Replace the comma with a period
-	}
+	} else if commaCount == 1 && periodCount == 1 {
+		// Case: Ambiguous format, need to determine based on positions
+		if strings.LastIndex(s, ",") > strings.LastIndex(s, ".") {
+			// European format
+			s = strings.Replace(s, ".", "", -1) // Remove all periods
+			s = strings.Replace(s, ",", ".", 1) // Replace the comma with a period
+		} else {
+			// US format
+			s = strings.Replace(s, ",", "", -1) // Remove all commas
+		}
+	} else if commaCount > 0 && periodCount == 0 {
+		// Case: Only commas, assume European format with comma as decimal separator
+		s = strings.Replace(s, ",", ".", 1) // Replace the comma with a period
+	} /*else if periodCount > 0 && commaCount == 0 {
+		// Case: Only periods, assume US format
+		// No action needed, already in correct format
+	}*/
+
 	return s
 }
